7febd394549c8d94d8d5331180adac4b72f2cca5
Meek Msaki <meek10x@gmail.com>
2024-03-17 10:44:49 -0500

    add amount_out module and update main.rs

--- src/amount_out.rs
+++ src/amount_out.rs
@@ -1,7 +1,7 @@
 pub fn run() {
     // before transaction
-    let x: f32 = 200.0; // amount tokens x in pool
-    let y: f32 = 800_000.0; // amount tokens y in pool
+    let x: f32 = 10_000.0; // amount tokens x in pool
+    let y: f32 = 40_000_000.0; // amount tokens y in pool
     let fee = 1.0 - 0.003; // uniswap fee
     let l = x * y; // constant L
 
@@ -9,10 +9,10 @@ pub fn run() {
     println!("x={}ETH, y=${}, L={}", x, y, l);
 
     // after transaction
-    let lambda: f32 = 200_000.0; // amount in of token y
-    let amount_in = (x * fee * lambda) / (y + fee * lambda);
-    let x1 = x - amount_in; // amount tokens x after
-    let y1 = y + lambda; // amount tokens y after
+    let delta: f32 = 500.0; // amount out of token x
+    let amount_out = (delta * y) / (fee * (x - delta));
+    let x1 = x - delta; // amount tokens x after
+    let y1 = y + amount_out; // amount tokens y after
 
     let li = x1 * y1; // constant L after trade
 
@@ -23,7 +23,7 @@ pub fn run() {
 
     // Impact of trade
     let p = y / x; // spot price
-    let pe = lambda / (x - x1); // effective price
+    let pe = amount_out / (x - x1); // effective price
     let impact = pe * (x - x1) - p * (x - x1); // price impact of trade
     let pi = y1 / x1; // new price after trade
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+mod amount_out;
 mod swap_math;
 mod swap_no_fees;
 fn main() {
     println!("-------- swap w/o fees ---------");
     swap_no_fees::run();
 
-    println!("\n-------- swap with fees ---------");
+    println!("\n-------- swap: amount in ---------");
     swap_math::run();
+
+    println!("\n-------- swap: amount out ---------");
+    amount_out::run();
 }
