2c4ebfe29d7753e3f08e1e2dec22990c096b15fb
Meek Msaki <meek10x@gmail.com>
2024-03-17 09:01:38 -0500

    Add swap_math and swap_no_fees modules

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
+mod swap_math;
+mod swap_no_fees;
 fn main() {
-    // before transaction
-    let x = 200; // amount tokens x in pool
-    let y = 800_000; // amount tokens y in pool
+    println!("-------- no fees math ---------");
+    swap_no_fees::run();
 
-    let l = x * y; // constant L
-
-    println!("x={}ETH, y=${}, L={}", x, y, l);
-
-    // after transaction
-    let lambda = 200_000; // amount in of token y
-    let b = y + lambda; // amount tokens y after
-    let a = l / b; // amount tokens x after
-
-    let li = a * b; // constant L after trade
-
-    assert_eq!(l, li); // check invariant
-
-    println!("x={}ETH, y=${}, L={}", a, b, li);
-
-    // Impact of trade
-    let p = y / x; // spot price
-    let pe = lambda / (x - a); // effective price
-    let impact = lambda - p * (x - a); // price impact of trade
-    let pi = b / a; // price after trade
-    println!("p=${}, pe=${}, impact=${}, pi=${}", p, pe, impact, pi);
+    println!("\n-------- swap w/ fees ---------");
+    swap_math::run();
 }
---
+++ src/swap_no_fees.rs
@@ -0,0 +1,26 @@
+pub fn run() {
+    // before transaction
+    let x = 200; // amount tokens x in pool
+    let y = 800_000; // amount tokens y in pool
+    let l = x * y; // constant L
+
+    println!("x={}ETH, y=${}, L={}", x, y, l);
+
+    // after transaction
+    let lambda = 200_000; // amount in of token y
+    let b = y + lambda; // amount tokens y after
+    let a = l / b; // amount tokens x after
+
+    let li = a * b; // constant L after trade
+
+    assert_eq!(l, li); // check invariant
+
+    println!("x={}ETH, y=${}, L={}", a, b, li);
+
+    // Impact of trade
+    let p = y / x; // spot price
+    let pe = lambda / (x - a); // effective price
+    let impact = lambda - p * (x - a); // price impact of trade
+    let pi = b / a; // price after trade
+    println!("p=${}, pe=${}, impact=${}, pi=${}", p, pe, impact, pi);
+}
