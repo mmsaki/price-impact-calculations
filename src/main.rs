cd9cef9b2e23fc595228d9766225cd82988018bc
Meek Msaki <meek10x@gmail.com>
2024-03-19 18:32:35 -0500

    add amount_in module and update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+mod amount_in;
 mod amount_out;
-mod swap_math;
 mod swap_no_fees;
 fn main() {
     println!("-------- swap w/o fees ---------");
     swap_no_fees::run();
 
     println!("\n-------- swap: amount in ---------");
-    swap_math::run();
+    amount_in::run();
 
     println!("\n-------- swap: amount out w/ 2 consecutive trades ---------");
     amount_out::run();
